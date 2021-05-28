555694ad43d440e23dc8704c1abad598c228f1e1
iqbal <muhammad.mahendra@students.amikom.ac.id>
2021-05-28 22:29:38 +0700

    Initial commit

---
+++ bin/Increment_dan_Decrement.dart
@@ -0,0 +1,45 @@
+void main(){
+
+  /*
+  Increment digunakan untuk menambah variabel sebanyak 1 angka
+  Decrement digunakan untuk mengurangi variabel sebanyak 1 angka
+  */
+
+  var a = 0;
+  var i = 10;
+
+  var result = ++a;
+  // Pre Increment
+  /*
+   Tambah nilai variabel a sebanyak 1,
+   lalu kirim nilai variabel a
+   */
+
+  // Post Increment
+  var result1 = a++;
+  /*
+   Kita kirim nilai variabel a,
+   lalu kita tambah variabel a sebanyak 1
+   */
+
+  var result3 = --i;
+  // Pre Decrement
+  /*
+   Kurangi nilai variabel i sebanyak 1,
+   lalu kirim nilai variabel i
+   */
+
+  // Post Decrement
+  var result4 = i--;
+  /*
+   Kita kirim nilai variabel i,
+   lalu kita tambah variabel i sebanyak 1
+   */
+  print('Hasil dari pre increment a adalah = $result');
+  print('Hasil dari post increment a adalah = $result1');
+  print('Hasil dari pre decrement a adalah = $result3');
+  print('Hasil dari post decrement a adalah = $result4');
+
+  print('${a++}');
+  print('${i--}');
+}
